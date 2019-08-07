18abd5653e38b7f2d01ce13a3bc3fcb56e2c5bfe
Ilja Strobbe <ilja@robonaut.be>
2019-08-07 12:43:24 +0200

    Refactor js code

--- App.js
+++ App.js
@@ -1,334 +1,219 @@
 import RNSentiance from "react-native-sentiance";
-import React, {
-    Component
-} from "react";
+import React, { Component } from "react";
 import {
-    Platform,
-    StyleSheet,
-    Text,
-    View,
-    FlatList,
-    ScrollView,
-    TouchableOpacity,
-    Clipboard,
-    NativeEventEmitter,
-    Dimensions,
-    PermissionsAndroid
+  Platform,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  Clipboard,
+  NativeEventEmitter,
+  PermissionsAndroid
 } from "react-native";
+import styles from "./styles";
 
 rnSentianceEmitter = new NativeEventEmitter(RNSentiance);
-type Props = {};
 
-export default class App extends Component < Props > {
-    constructor() {
-        super();
-        this.state = {
-            userId: "...",
-            sdkVersion: "...",
-            userActivity: "...",
-            data: []
-        };
+export default class App extends Component {
+  state = {
+    userId: "...",
+    sdkVersion: "...",
+    userActivityText: "...",
+    data: []
+  };
 
-        this.setupSdk();
+  componentDidMount() {
+    this.setupSdk();
+    this.subscribe();
+    this.requestPermissionAndroid();
+  }
+
+  componentWillUnmount() {
+    this.unSubscribe();
+  }
+
+  unSubscribe() {
+    this.sdkStatusSubscription.remove();
+    this.sdkUserActivityUpdateSubscribtion.remove();
+  }
+
+  subscribe() {
+    this.sdkStatusSubscription = rnSentianceEmitter.addListener(
+      "SDKStatusUpdate",
+      sdkStatus => this.onSdkStatusUpdate(sdkStatus)
+    );
+
+    this.sdkUserActivityUpdateSubscribtion = rnSentianceEmitter.addListener(
+      "SDKUserActivityUpdate",
+      userActivity => {
+        this.onUserActivityUpdate(userActivity);
+      }
+    );
+  }
+
+  async setupSdk() {
+    const sdkInitialized = await this.isSdkInitialized();
+    if (sdkInitialized) {
+      const sdkStatus = await RNSentiance.getSdkStatus();
+      const userId = await RNSentiance.getUserId();
+      const sdkVersion = await RNSentiance.getVersion();
+      const data = await this.statusToData(sdkStatus);
+      this.setState({ userId, sdkVersion, data });
+      RNSentiance.listenUserActivityUpdates();
     }
+  }
 
-    componentDidMount() {
-        this.subscribe();
-        this.requestPermissionAndroid();
+  async onSdkStatusUpdate(sdkStatus) {
+    const data = await this.statusToData(sdkStatus);
+    this.setState({
+      data: data
+    });
+  }
+
+  onUserActivityUpdate(userActivity) {
+    const { type, stationaryInfo } = userActivity;
+    let userActivityText = "";
+
+    if (type === "USER_ACTIVITY_TYPE_STATIONARY") {
+      const { location } = stationaryInfo;
+      if (location) {
+        const { latitude, longitude } = location;
+        stationaryLocation = `${parseFloat(latitude)},${parseFloat(longitude)}`;
+      }
+      userActivityText = `Stationary @${stationaryLocation}`;
+    } else if (type === "USER_ACTIVITY_TYPE_TRIP") {
+      userActivityText = "Trip";
+    } else if (type === "USER_ACTIVITY_TYPE_UNKNOWN") {
+      userActivityText = "Unknown";
     }
+    this.setState({
+      userActivityText
+    });
+  }
 
-    componentWillUnmount() {
-        this.unSubscribe();
-    }
+  async isSdkInitialized() {
+    const initState = await RNSentiance.getInitState();
+    return initState == "INITIALIZED";
+  }
 
-    unSubscribe() {
-        this.sdkStatusSubscription.remove();
-        this.sdkUserActivityUpdateSubscribtion.remove();
-    }
+  async shouldInitialize() {
+    const initState = await RNSentiance.getInitState();
+    return initState == "NOT_INITIALIZED";
+  }
 
-    subscribe() {
-        this.sdkStatusSubscription = rnSentianceEmitter.addListener(
-            "SDKStatusUpdate",
-            sdkStatus => this.onSdkStatusUpdate(sdkStatus)
-        );
+  copyUserIdToBuffer = () => {
+    Clipboard.setString(this.state.userId);
+  };
 
-        this.sdkUserActivityUpdateSubscribtion = rnSentianceEmitter.addListener(
-            "SDKUserActivityUpdate",
-            userActivity => {
-                this.onUserActivityUpdate(userActivity);
-            }
-        );
-    }
-
-   async setupSdk() {
-        const shouldInitialize = await this.shouldInitialize();
-        if (shouldInitialize) {
-            await RNSentiance.init(
-                "APP_ID",
-                "APP_SECRET",
-                null,
-                true,
-            );
-        }
-
+  async requestPermissionAndroid() {
+    if (Platform.OS === "android") {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
         const sdkInitialized = await this.isSdkInitialized();
         if (sdkInitialized) {
-            const sdkStatus = await RNSentiance.getSdkStatus();
-            this.onSdkStatusUpdate(sdkStatus);
-            this.updateUserIdAndSdkVersion();
-            this.listenToUserActivity();
+          const sdkStatus = await RNSentiance.getSdkStatus();
+          this.onSdkStatusUpdate(sdkStatus);
         }
-    }
-
-    async onSdkStatusUpdate(sdkStatus) {
-      const data = await this.statusToData(sdkStatus);
-      this.setState({
-        data: data
-      });
-
-      if (this.isSdkInitialized()) {
-        this.updateUserIdAndSdkVersion();
-        this.listenToUserActivity();
       }
     }
+  }
 
-    onUserActivityUpdate(userActivity) {
-        const {
-            type,
-            tripInfo,
-            stationaryInfo
-        } = userActivity;
+  async statusToData(sdkStatus) {
+    const {
+      startStatus,
+      isRemoteEnabled,
+      isLocationPermGranted,
+      locationSetting,
+      isAccelPresent,
+      isGyroPresent,
+      isGooglePlayServicesMissing,
+      wifiQuotaStatus,
+      mobileQuotaStatus,
+      diskQuotaStatus
+    } = sdkStatus;
 
-        if (type === "USER_ACTIVITY_TYPE_STATIONARY") {
-            const {
-                location
-            } = stationaryInfo;
-            if (location) {
-                const {
-                    latitude,
-                    longitude
-                } = location;
-                stationaryLocation = parseFloat(latitude) + "," + parseFloat(longitude);
-            }
+    const diskQuota = await RNSentiance.getDiskQuotaLimit();
+    const diskQuotaUsed = await RNSentiance.getDiskQuotaUsage();
+    const mobileQuota = await RNSentiance.getMobileQuotaLimit();
+    const mobileQuotaUsed = await RNSentiance.getMobileQuotaUsage();
+    const wifiQuota = await RNSentiance.getWiFiQuotaLimit();
+    const wifiQuotaUsed = await RNSentiance.getWiFiQuotaUsage();
 
-            info = "Stationary  @" + stationaryLocation;
-            this.setState({
-                userActivity: info
-            });
-        } else if (type === "USER_ACTIVITY_TYPE_TRIP") {
-            this.setState({
-                userActivity: "Trip"
-            });
-        } else if (type === "USER_ACTIVITY_TYPE_UNKNOWN") {
-            this.setState({
-                userActivity: "Unknown"
-            });
-        }
-    }
+    data = [
+      {
+        key: "startStatus",
+        value: startStatus
+      },
+      {
+        key: "isRemoteEnabled",
+        value: isRemoteEnabled ? "YES" : "NO"
+      },
+      {
+        key: "isLocationPermGranted",
+        value: isLocationPermGranted ? "YES" : "NO"
+      },
+      {
+        key: "locationSetting",
+        value: locationSetting ? "YES" : "NO"
+      },
+      {
+        key: "isAccelPresent",
+        value: isAccelPresent ? "YES" : "NO"
+      },
+      {
+        key: "isGyroPresent",
+        value: isGyroPresent ? "YES" : "NO"
+      },
+      {
+        key: "isGooglePlayServicesMissing",
+        value: isGooglePlayServicesMissing ? "YES" : "NO"
+      },
+      {
+        key: "wifiQuotaStatus",
+        value: `${wifiQuotaStatus} (${wifiQuotaUsed}/${wifiQuota} )`
+      },
+      {
+        key: "mobileQuotaStatus",
 
-    async updateUserIdAndSdkVersion() {
-        try {
-            const userId = await RNSentiance.getUserId();
-            this.setState({userId: userId});
-            this.setState({sdkVersion: await RNSentiance.getVersion()});
-        } catch (err) {
+        value: `${mobileQuotaStatus} (${mobileQuotaUsed}/${mobileQuota} )`
+      },
+      {
+        key: "diskQuotaStatus",
+        value: `${diskQuotaStatus} (${diskQuotaUsed}/${diskQuota} )`
+      }
+    ];
 
-        }
-    }
+    return data;
+  }
 
-    async isSdkInitialized() {
-        const initState = await RNSentiance.getInitState();
-        return initState == "INITIALIZED";
-    }
+  render() {
+    const { userId, sdkVersion, userActivityText, data } = this.state;
 
-    async shouldInitialize() {
-        const initState = await RNSentiance.getInitState();
-        return initState == "NOT_INITIALIZED";
-    }
-
-    async listenToUserActivity() {
-        //listen to user activity changes
-        RNSentiance.listenUserActivityUpdates();
-        const activity = await RNSentiance.getUserActivity();
-        this.onUserActivityUpdate(activity);
-    }
-
-    copyUserIdToBuffer = () => {
-        Clipboard.setString(this.state.userId);
-    }
-
-    async requestPermissionAndroid(){
-        if(Platform.OS === 'android'){
-            const granted = await PermissionsAndroid.request(
-                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
-                )
-            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-                
-                const sdkInitialized = await this.isSdkInitialized();
-                if (sdkInitialized) {
-                    const sdkStatus = await RNSentiance.getSdkStatus();
-                    this.onSdkStatusUpdate(sdkStatus);
-                }
-            }
-        }
-    }
-
-    async statusToData(sdkStatus) {
-      const isInitialized = await this.isSdkInitialized();
-        const {
-            startStatus,
-            isRemoteEnabled,
-            isLocationPermGranted,
-            locationSetting,
-            isAccelPresent,
-            isGyroPresent,
-            isGooglePlayServicesMissing,
-            wifiQuotaStatus,
-            mobileQuotaStatus,
-            diskQuotaStatus
-        } = sdkStatus;
-
-        diskQuota = !isInitialized ? '--' : await RNSentiance.getDiskQuotaLimit();
-        diskQuotaUsed = !isInitialized ? '--' : await RNSentiance.getDiskQuotaUsage();
-
-        mobileQuota = !isInitialized ? '--' : await RNSentiance.getMobileQuotaLimit();
-        mobileQuotaUsed = !isInitialized ? '--' : await RNSentiance.getMobileQuotaUsage();
-
-        wifiQuota = !isInitialized ? '--' : await RNSentiance.getWiFiQuotaLimit();
-        wifiQuotaUsed = !isInitialized ? '--' : await RNSentiance.getWiFiQuotaUsage();
-
-        data = [{
-                key: "startStatus",
-                value: startStatus
-            },
-            {
-                key: "isRemoteEnabled",
-                value: isRemoteEnabled ? "YES" : "NO"
-            },
-            {
-                key: "isLocationPermGranted",
-                value: isLocationPermGranted ? "YES" : "NO"
-            },
-            {
-                key: "locationSetting",
-                value: locationSetting ? "YES" : "NO"
-            },
-            {
-                key: "isAccelPresent",
-                value: isAccelPresent ? "YES" : "NO"
-            },
-            {
-                key: "isGyroPresent",
-                value: isGyroPresent ? "YES" : "NO"
-            },
-            {
-                key: "isGooglePlayServicesMissing",
-                value: isGooglePlayServicesMissing ? "YES" : "NO"
-            },
-            {
-                key: "wifiQuotaStatus",
-                value: wifiQuotaStatus + "  (" + wifiQuotaUsed + "/" + wifiQuota + ")"
-            },
-            {
-                key: "mobileQuotaStatus",
-                value: mobileQuotaStatus + "  (" + mobileQuotaUsed + "/" + mobileQuota + ")"
-            },
-            {
-                key: "diskQuotaStatus",
-                value: diskQuotaStatus + "  (" + diskQuotaUsed + "/" + diskQuota + ")"
-            }
-        ];
-
-        return data;
-      
-    }
-
-   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}> RNSentiance </Text>
-        <Text style={styles.valueStyle}> {this.state.userId} </Text>
-        <Text style={styles.sdkVersion}> SDK version: {this.state.sdkVersion}</Text>
+        <Text style={styles.welcome}>RNSentiance</Text>
+        <Text style={styles.heading}>User ID</Text>
+        <Text style={styles.valueStyle}>{userId}</Text>
+        <Text style={styles.sdkVersion}>SDK version:{sdkVersion}</Text>
         <TouchableOpacity
-          onPress={this.copyUserIdToBuffer.bind()}
+          onPress={e => this.copyUserIdToBuffer()}
           underlayColor="#fff"
         >
-        <Text style={styles.copyButton}> copy </Text>
+          <Text style={styles.copyButton}>copy</Text>
         </TouchableOpacity>
-        <Text style={styles.heading}> User Activity </Text>
-        <Text style={styles.valueStyle}> {this.state.userActivity} </Text>
-        <Text style={styles.heading}> SDK Status </Text>
-        <ScrollView>
-          <FlatList
-            style={styles.sdkStatusList}
-            data={this.state.data}
-            renderItem={({ item }) => (
-              <View>
-                <Text style={styles.valueStyle}>{item.key}: {item.value}</Text>
-              </View>
-            )}
-          />
+        <Text style={styles.heading}>User Activity</Text>
+        <Text style={styles.valueStyle}> {userActivityText} </Text>
+        <Text style={styles.heading}>SDK Status</Text>
+        <ScrollView contentContainerStyle={styles.sdkStatusList}>
+          {data.map(item => (
+            <Text key={`item-${item.key}`} style={styles.valueStyle}>
+              {item.key}: {item.value}
+            </Text>
+          ))}
         </ScrollView>
       </View>
     );
   }
-
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "flex-start",
-    backgroundColor: "black"
-  },
-  sdkStatusList: {
-    width: Dimensions.get("window").width,
-    borderRadius: 4,
-    borderWidth: 0.5,
-    borderColor: "#d6d7da",
-    color: "white",
-    textAlign: "left",
-    flexGrow: 0
-  },
-  welcome: {
-    width: Dimensions.get("window").width,
-    marginTop: 30,
-    fontSize: 40,
-    textAlign: "center",
-    margin: 10,
-    color: "white",
-    fontWeight: "bold"
-  },
-  valueStyle: {
-    textAlign: "left",
-    fontSize: 15,
-    color: "white",
-    marginBottom: 5,
-    marginLeft: 10
-  },
-  heading: {
-    marginTop: 10,
-    textAlign: "left",
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "yellow",
-    marginBottom: 5,
-    marginLeft: 10
-  },
-  copyButton: {
-    marginTop: 0,
-    padding: 5,
-    backgroundColor: "red",
-    color: "white",
-    fontSize: 15,
-    marginLeft: 10
-  },
-  sdkVersion: {
-    width: Dimensions.get("window").width,
-    position: "absolute",
-    bottom: 15,
-    color: "gray",
-    textAlign: "center"
-  }
-});
